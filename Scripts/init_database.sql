f43b8787e99e1b20921a76bee273ae82b386a7f7
Daniele1388 <danieleamoroso13@gmail.com>
2025-08-03 13:10:33 +0200

    Create init_database.sql

---
+++ Scripts/init_database.sql
@@ -0,0 +1,33 @@
+/*
+===============================================
+Create DataBase 'TourismDB' and Schemas
+===============================================
+*/
+
+USE master;
+GO
+
+-- Check if the TourismDB database already exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'TourismDB')
+BEGIN
+	ALTER DATABASE TourismDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE TourismDB;
+END;
+GO
+
+-- Create the 'TourismDB'
+CREATE DATABASE TourismDB;
+GO
+
+USE TourismDB;
+GO
+
+-- Create Schemas
+CREATE SCHEMA bronze;
+GO
+
+CREATE SCHEMA silver;
+GO
+
+CREATE SCHEMA gold;
+GO
