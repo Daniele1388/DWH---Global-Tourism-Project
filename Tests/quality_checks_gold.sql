8568b8bd9e82903d1bdc7d7f18d6c0074418b883
Daniele1388 <danieleamoroso13@gmail.com>
2025-09-04 19:45:28 +0200

    Create quality_checks_gold.sql

---
+++ Tests/quality_checks_gold.sql
@@ -0,0 +1,41 @@
+==================================================================================
+-- checking 'gold.fact_domestic_tourism'; 'gold.fact_inbound_tourism'; 
+  -- 'gold.fact_outbound_tourism';'gold.fact_tourism_industries'
+==================================================================================
+--   Foreign Key Integrity (Dimensions) 
+--   To use with Domestic/Inbound/Outbound/Industries, change the fact table in FROM.
+  
+	SELECT
+	*
+	FROM gold.fact_domestic_tourism f
+	LEFT JOIN gold.dim_country c
+	ON c.Country_key = f.Country_key
+	LEFT JOIN gold.dim_indicator i
+	ON i.Indicator_key = f.Indicator_key
+	LEFT JOIN gold.dim_year y
+	ON y.Year_key = f.Year_key
+	LEFT JOIN gold.dim_unit_of_measure u
+	ON u.Units_key = f.Units_key
+	WHERE	c.Country_key IS NULL
+			OR i.Indicator_key IS NULL
+			OR y.Year_key IS NULL 
+			OR u.Units_key IS NULL
+
+==================================================================================
+-- Checking 'gold.fact_sdg'
+==================================================================================
+--   Foreign Key Integrity (Dimensions) 
+--   To use with SDG
+  
+  SELECT
+	*
+	FROM gold.fact_sdg f
+	LEFT JOIN gold.dim_country c
+	ON c.Country_key = f.Country_key
+	LEFT JOIN gold.dim_year y
+	ON y.Year_key = f.Year_key
+	LEFT JOIN gold.dim_unit_of_measure u
+	ON u.Units_key = f.Units_key
+	WHERE	c.Country_key IS NULL
+			OR y.Year_key IS NULL 
+			OR u.Units_key IS NULL
